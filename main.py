d74a8bf27c50b09889816d3ed9b75a72db404c31
TylerHager2 <tylerhager02@outlook.com>
2024-06-28 08:09:45 -0700

    test1

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ def main():
     Code Your Program here
     ########################################
     """
+    
+    if num1 > num2 and num1 > num3:
+        maxval = num1
+        if num2 > num3:
+            minval = num3
+            median = num2
+        else:
+            minval = num2
+            median = num3
+    elif num2 > num3:
+        maxval = num2
+        if num1 > num3:
+            minval = num3
+            median = num1
+        else:
+            minval = num1
+            median = num3
+    else:
+        maxval = num3
+        if num1 > num2:
+            minval = num2
+            median = num1
+        else:
+            minval = num1
+            median = num2
+            
 
     print(minval, median, maxval)
     ########################################
